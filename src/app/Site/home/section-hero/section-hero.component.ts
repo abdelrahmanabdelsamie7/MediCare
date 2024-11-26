157f907dc972740fee53173fc934c4e69e0dbd0a
abdelrahmanabdelsamie7 <abdelrhmanabdelsamie@gmail.com>
2024-11-26 23:01:26 +0200

    second commit

---
+++ src/app/Site/home/section-hero/section-hero.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+@Component({
+  selector: 'app-section-hero',
+  standalone: true,
+  imports: [CarouselModule],
+  templateUrl: './section-hero.component.html',
+  styleUrl: './section-hero.component.css',
+})
+export class SectionHeroComponent {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    margin: 10,
+    navSpeed: 700,
+    navText: [
+      '<i class="fa-chevron-left"></i>',
+      '<i class="fa-chevron-right></i>"',
+    ],
+    responsive: {
+      0: {
+        items: 2,
+      },
+    },
+    nav: true,
+  };
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SiteNavbarComponent } from './Site/shared/site-navbar/site-navbar.component';
+import { SectionHeroComponent } from './Site/home/section-hero/section-hero.component';
+import { SectionDepartmentsComponent } from './Site/home/section-departments/section-departments.component';
+import { SectionPharmaciesComponent } from './Site/home/section-pharmacies/section-pharmacies.component';
+import { SectionLaboratoriesComponent } from './Site/home/section-laboratories/section-laboratories.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    SiteNavbarComponent,
+    SectionHeroComponent,
+    SectionDepartmentsComponent,
+    SectionPharmaciesComponent,
+    SectionLaboratoriesComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'MediCare';
