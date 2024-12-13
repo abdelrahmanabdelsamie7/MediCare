d86bdb5b2f8f46fc2ba06d68a1b49da27a22da14
abdelrahmanabdelsamie7 <abdelrhmanabdelsamie@gmail.com>
2024-12-13 23:50:12 +0200

    Update Carsoual

--- src/app/Site/home/section-hero/section-hero.component.ts
+++ src/app/Site/home/section-hero/section-hero.component.ts
@@ -1,36 +1,63 @@
 import { Component } from '@angular/core';
-import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
-import { Carousel } from 'primeng/carousel';
+import { Carousel, CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-section-hero',
   standalone: true,
-  imports: [CarouselModule,Carousel],
+  imports: [
+    CarouselModule,
+    ButtonModule,
+    TagModule,
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './section-hero.component.html',
   styleUrl: './section-hero.component.css',
 })
 export class SectionHeroComponent {
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    margin: 10,
-    navSpeed: 700,
-    navText: [
-      '<i class="fa-chevron-left"></i>',
-      '<i class="fa-chevron-right"></i>',
-    ],
-    responsive: {
-      0: {
-        items: 2,
-      },
+  responsiveOptions: any[] | undefined;
+  Images = [
+    {
+      imageUrl: './hero1.jpg',
     },
-    nav: true,
-  };
-  images: string[] = [
-    'hero1.jpg',
-    'hero1.jpg',
-    'hero1.jpg',
+    {
+      imageUrl: './hero1.jpg',
+    },
+    {
+      imageUrl: './hero1.jpg',
+    },
+    {
+      imageUrl: './hero1.jpg',
+    },
+    {
+      imageUrl: './hero1.jpg',
+    },
   ];
+  constructor() {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,31 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
+// Routes
 import { routes } from './app.routes';
+// Http Client Import
+import { provideHttpClient, withFetch } from '@angular/common/http';
+// Start Of Import Prime Ng Plugins
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-import { provideClientHydration } from '@angular/platform-browser';
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
-
+// End Of mport Prime Ng Plugins
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
     provideAnimationsAsync(),
     providePrimeNG({
-        theme: {
-            preset: Aura
-        }
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
+      },
     }),
-provideAnimations(),
-    provideClientHydration()]
+  ],
 };
