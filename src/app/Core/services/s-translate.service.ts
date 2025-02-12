ae2919014abee3839f6afa5b3b926e216765c1ff
[200~Mayadamohamed1 <mayabdelzaher290@gmail.com>
2025-02-12 22:45:31 +0200

    Add the home page to the navbar and set direction

--- src/app/Core/services/s-translate.service.ts
+++ src/app/Core/services/s-translate.service.ts
@@ -1,33 +1,32 @@
 import { inject, Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class STranslateService {
 private readonly _TranslateService= inject( TranslateService)
+  private langSubject = new BehaviorSubject<string>(localStorage.getItem('lang') || 'ar');
+  currentLang$ = this.langSubject.asObservable();
   constructor() {
-    //1-get lang local -- words
-    let saveLang = localStorage.getItem('lang') || 'ar';//lang->select (en , ar)
-  //2-setDefault Lang
-this._TranslateService.setDefaultLang('ar')
-//3-use lang-->local
-this._TranslateService.use(saveLang !)
-
+    let saveLang = localStorage.getItem('lang') || 'ar'; //lang->select (en , ar)
+    //2-setDefault Lang
+    this._TranslateService.setDefaultLang('ar');
+    this._TranslateService.use(saveLang);
+    // this.changeDirection(saveLang);
+  }
+  changeLang(lang: string): void {
+    localStorage.setItem('lang', lang); //save local
+    this._TranslateService.use(lang); // use lang-->local
+    this.langSubject.next(lang); // تحديث لكل
+    // this.changeDirection(lang);
+  }
 //Direction ---->en->ltr  , ar->rtl
-//this.changeDirection()
-}
-
-// changeDirection():void{
-//   let saveLang = localStorage.getItem('lang')
-//   if(saveLang==='en')  {// dir->ltr
-//     document.documentElement.dir ='ltr'
-//     }else if(saveLang==='ar'){// dir->rtl
-//     document.documentElement.dir ='rtl'
-//     }
-// }
-changeLang(lang:string):void{
-  localStorage.setItem('lang', lang) //save in local
-  this._TranslateService.use(lang)
-  //this.changeDirection()
-}
+  // private changeDirection(lang: string): void {
+  //   if (lang === 'ar') {
+  //     document.documentElement.dir = 'rtl';
+  //   } else {
+  //     document.documentElement.dir = 'ltr';
+  //   }
+  // }
 }
