ab0ffc6cf370088adfd2bc42f1d18ee46aba113d
abdelrahmanabdelsamie7 <abdelrhmanabdelsamie@gmail.com>
2025-06-17 03:27:10 +0300

    some update to discution ..

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://192.168.1.4:8000/api',
+  baseUrl: 'http://192.168.66.123:8000/api',
   googleClientId: '92169134411-28qrk92idr4hklh75i2jk0i10tll6643.apps.googleusercontent.com',
   stripePublicKey: 'pk_test_51QoYFFK0gkrS4a8OwDnUnitNjiLNGUAi9TR3Y1XGW2fnhG7XYad3ZlqgvQPqfG6GF4vypoRRQkMYGxDysUZfts7w00tqmPFCvo',
 };
